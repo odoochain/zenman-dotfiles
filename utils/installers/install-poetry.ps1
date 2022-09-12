4a292338aed5cf8debcc802ef247908f2d0e502c
mino29 <42552577+mino29@users.noreply.github.com>
2022-09-12 20:03:11 +0800

    add install-poetry.ps1

---
+++ utils/installers/install-poetry.ps1
@@ -0,0 +1,35 @@
+
+# This make sure you run as Administrator
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))  
+{  
+  $arguments = "& '" +$myinvocation.mycommand.definition + "'"
+  Start-Process powershell -Verb runAs -ArgumentList $arguments
+  Break
+}
+
+
+Try{
+  poetry --version
+  Write-host "poetry is installed on your system."
+}
+Catch
+{
+# Make sure this can install PS modules
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+
+# Install poetry from official script
+(Invoke-WebRequest -Uri https://install.python-poetry.org/ -UseBasicParsing).Content | python -
+
+$Env:PATH = "C:\Users\mino29\AppData\Roaming\Python\Scripts\;$Env:PATH"
+Write-host "Poetry has been add to path. You can run `poetry --version` to test it out."
+}
+
+
+
+
+
+
+
+
+
+# $Env:PATH = "C:\Users\mino29\AppData\Roaming\Python\Python39\Scripts;$Env:PATH"
