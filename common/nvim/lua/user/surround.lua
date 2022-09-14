b909150f2e754af1c0c177349be3b8c026d152dc
mino29 <42552577+mino29@users.noreply.github.com>
2022-09-15 00:31:09 +0800

    replace meovim as a submodule

--- common/nvim/lua/user/surround.lua
+++
@@ -1,8 +0,0 @@
--- plugin for quick surround feature
-vim.cmd[[
-    vmap Si S(i_<esc>f)
-    au FileType mako vmap Si S"i${ _(<esc>2f"a) }<esc>
-]]
-
--- new-found features
--- press g then ctrl-g, shows the word count of current buffer
