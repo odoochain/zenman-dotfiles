f3c1a197d12cd0ce72e5a1354e8c313d7eb98cd4
mino29 <42552577+mino29@users.noreply.github.com>
2022-09-14 18:12:39 +0800

    powershell stuff

--- windows/ambient/mappings.ps1
+++ windows/ambient/mappings.ps1
@@ -10,110 +10,110 @@ if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 
 # git
-New-Item -Path "C:$env:HOMEPATH\.gitconfig" -ItemType SymbolicLink -Target "C:$env:HOMEPATH\.dotfiles\common\git\gitconfig" -Force
+New-Item -Path "$env:USERPROFILE\.gitconfig" -ItemType SymbolicLink -Target "$env:USERPROFILE\.dotfiles\common\git\gitconfig" -Force
 Write-host ".gitconfig has been linked to .dotfiles\common\git\gitconfig"
 
 # mintty-terminal(that comes with git-bash)
-New-Item -Path "C:$env:HOMEPATH\.minttyrc" -ItemType SymbolicLink -Target "C:$env:HOMEPATH\.dotfiles\windows\mintty\.minttyrc" -Force
+New-Item -Path "$env:USERPROFILE\.minttyrc" -ItemType SymbolicLink -Target "$env:USERPROFILE\.dotfiles\windows\mintty\.minttyrc" -Force
 Write-host ".minttyrc has been linked to .dotfiles\windows\mintty\.minttyrc"
 
 # Window-Terminal
-New-Item -Path "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -ItemType SymbolicLink -Target "C:$env:HOMEPATH\.dotfiles\windows\windows-terminal\settings.json" -Force
+New-Item -Path "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -ItemType SymbolicLink -Target "$env:USERPROFILE\.dotfiles\windows\windows-terminal\settings.json" -Force
 Write-host "windows terminal settings has been linked to .dotfiles\windows\windows-terminal\settings.json"
 
 # windows powershell(5.1)
-New-Item -Path "C:$env:HOMEPATH\Documents\WindowsPowershell" -ItemType SymbolicLink -Target "C:$env:HOMEPATH\.dotfiles\windows\powershell" -Force
+New-Item -Path "$env:USERPROFILE\Documents\WindowsPowershell" -ItemType SymbolicLink -Target "$env:USERPROFILE\.dotfiles\windows\powershell" -Force
 Write-host "WindowsPowershell profile config has been linked to .dotfiles\windows\powershell\"
 
 # powershell7
-New-Item -Path "C:$env:HOMEPATH\Documents\Powershell" -ItemType SymbolicLink -Target "C:$env:HOMEPATH\.dotfiles\windows\powershell" -Force
+New-Item -Path "$env:USERPROFILE\Documents\Powershell" -ItemType SymbolicLink -Target "$env:USERPROFILE\.dotfiles\windows\powershell" -Force
 Write-host "powershell7 profile config has been linked to .dotfiles\windows\powershell\"
 
 # starship
-New-Item -Path "C:$env:HOMEPATH\.starship" -ItemType SymbolicLink -Target "C:$env:HOMEPATH\.dotfiles\common\.starship" -Force
+New-Item -Path "$env:USERPROFILE\.starship" -ItemType SymbolicLink -Target "$env:USERPROFILE\.dotfiles\common\.starship" -Force
 Write-host "~\.starship has been linked to .dotfiles\common\.starship"
 
 # Python/pip
-New-Item -Path "C:$env:HOMEPATH\pip" -ItemType SymbolicLink -Target "C:$env:HOMEPATH\.dotfiles\windows\pip" -Force
+New-Item -Path "$env:USERPROFILE\pip" -ItemType SymbolicLink -Target "$env:USERPROFILE\.dotfiles\windows\pip" -Force
 Write-host "~\pip has been linked to .dotfiles\windows\pip"
 
 # conda
-New-Item -Path "C:$env:HOMEPATH\.condarc" -ItemType SymbolicLink -Target "C:$env:HOMEPATH\.dotfiles\common\conda\.condarc" -Force
+New-Item -Path "$env:USERPROFILE\.condarc" -ItemType SymbolicLink -Target "$env:USERPROFILE\.dotfiles\common\conda\.condarc" -Force
 Write-host "~\.condarc has been linked to .dotfiles\common\conda\.condarc"
 
 # npm
-New-Item -Path "C:$env:HOMEPATH\.npmrc" -ItemType SymbolicLink -Target "C:$env:HOMEPATH\.dotfiles\common\npm\.npmrc" -Force
+New-Item -Path "$env:USERPROFILE\.npmrc" -ItemType SymbolicLink -Target "$env:USERPROFILE\.dotfiles\common\npm\.npmrc" -Force
 Write-host "~\.npmrc has been linked to .dotfiles\common\npm\.npmrc"
 
 
 # neovim
-New-Item -Path "$env:LOCALAPPDATA\nvim" -ItemType SymbolicLink -Target "C:$env:HOMEPATH\.dotfiles\common\nvim" -Force
+New-Item -Path "$env:LOCALAPPDATA\nvim" -ItemType SymbolicLink -Target "$env:USERPROFILE\.dotfiles\common\nvim" -Force
 Write-host "nvim has been linked to .dotfiles\common\nvim"
 
 # vim
-New-Item -Path "$home\.vimrc" -ItemType SymbolicLink -Target "C:$env:HOMEPATH\.dotfiles\common\vim\.vimrc" -Force
+New-Item -Path "$env:USERPROFILE\.vimrc" -ItemType SymbolicLink -Target "$env:USERPROFILE\.dotfiles\common\vim\.vimrc" -Force
 Write-host ".vimrc has been linked to .dotfiles\common\vim\.vimrc"
 
 # ideavim
-New-Item -Path "$home\.ideavimrc\" -ItemType SymbolicLink -Target "C:$env:HOMEPATH\.dotfiles\common\ideam\.ideavimrc" -Force
+New-Item -Path "$env:USERPROFILE\.ideavimrc\" -ItemType SymbolicLink -Target "$env:USERPROFILE\.dotfiles\common\ideavim\.ideavimrc" -Force
 Write-host "~\.ideavimrc has been linked to .dotfiles\common\ideam\.ideavimrc"
 
 # Winfetch
-New-Item -Path "C:$env:HOMEPATH\.config\winfetch\config.ps1" -ItemType SymbolicLink -Target "C:$env:HOMEPATH\.dotfiles\windows\winfetch\config.ps1" -Force
+New-Item -Path "$env:USERPROFILE\.config\winfetch\config.ps1" -ItemType SymbolicLink -Target "$env:USERPROFILE\.dotfiles\windows\winfetch\config.ps1" -Force
 Write-host "winfetch config has been linked to .dotfiles\windows\winfetch\config.ps1"
 
 # cmd/clink
-New-Item -Path "C:$env:HOMEPATH\alias.cmd" -ItemType SymbolicLink -Target "C:$env:HOMEPATH\.dotfiles\windows\cmd\alias.cmd" -Force
+New-Item -Path "$env:USERPROFILE\alias.cmd" -ItemType SymbolicLink -Target "$env:USERPROFILE\.dotfiles\windows\cmd\alias.cmd" -Force
 Write-host "alias.cmd has been linked to .dotfiles\windows\cmd\alias.cmd"
 
-New-Item -Path "C:\Users\Public\init.cmd" -ItemType SymbolicLink -Target "C:$env:HOMEPATH\.dotfiles\windows\cmd\init.cmd" -Force
+New-Item -Path "C:\Users\Public\init.cmd" -ItemType SymbolicLink -Target "$env:USERPROFILE\.dotfiles\windows\cmd\init.cmd" -Force
 Write-host "init.cmd has been linked to .dotfiles\windows\cmd\init.cmd"
 
-New-Item -Path "C:$env:HOMEPATH\scoop\apps\clink\current\oh-my-posh.lua" -ItemType SymbolicLink -Target "C:$env:HOMEPATH\.dotfiles\windows\cmd\oh-my-posh.lua" -Force
+New-Item -Path "$env:USERPROFILE\scoop\apps\clink\current\oh-my-posh.lua" -ItemType SymbolicLink -Target "$env:USERPROFILE\.dotfiles\windows\cmd\oh-my-posh.lua" -Force
 Write-host "oh-my-posh.lua has been linked to .dotfiles\windows\cmd\oh-my-posh.lua"
 
 # z.lua for cmd/clink
-New-Item -Path "C:$env:HOMEPATH\scoop\apps\clink\current\z.lua" -ItemType SymbolicLink -Target "C:$env:HOMEPATH\.dotfiles\windows\cmd\z.lua" -Force
+New-Item -Path "$env:USERPROFILE\scoop\apps\clink\current\z.lua" -ItemType SymbolicLink -Target "$env:USERPROFILE\.dotfiles\windows\cmd\z.lua" -Force
 Write-host "z.lua has been linked to .dotfiles\windows\cmd\z.lua"
 
-New-Item -Path "C:$env:HOMEPATH\scoop\apps\clink\current\z.cmd" -ItemType SymbolicLink -Target "C:$env:HOMEPATH\.dotfiles\windows\cmd\z.cmd" -Force
+New-Item -Path "$env:USERPROFILE\scoop\apps\clink\current\z.cmd" -ItemType SymbolicLink -Target "$env:USERPROFILE\.dotfiles\windows\cmd\z.cmd" -Force
 Write-host "z.cmd has been linked to .dotfiles\windows\cmd\z.cmd"
 
 # lf
-New-Item -Path "$env:LOCALAPPDATA\lf" -ItemType SymbolicLink -Target "C:$env:HOMEPATH\.dotfiles\common\lf" -Force
+New-Item -Path "$env:LOCALAPPDATA\lf" -ItemType SymbolicLink -Target "$env:USERPROFILE\.dotfiles\common\lf" -Force
 Write-host "~\lf has been linked to .dotfiles\windows\lf"
 
 # JPEGView
-New-Item -Path "$env:APPDATA\JPEGView" -ItemType SymbolicLink -Target "C:$env:HOMEPATH\.dotfiles\windows\JPEGView" -Force
+New-Item -Path "$env:APPDATA\JPEGView" -ItemType SymbolicLink -Target "$env:USERPROFILE\.dotfiles\windows\JPEGView" -Force
 Write-host "JPEGView has been linked to .dotfiles\windows\JPEGView"
 
 # Everything
-New-Item -Path "$env:APPDATA\Everything" -ItemType SymbolicLink -Target "C:$env:HOMEPATH\.dotfiles\windows\Everything" -Force
+New-Item -Path "$env:APPDATA\Everything" -ItemType SymbolicLink -Target "$env:USERPROFILE\.dotfiles\windows\Everything" -Force
 Write-host "Everything has been linked to .dotfiles\windows\Everything"
 
 # workspacer
-New-Item -Path "C:$env:HOMEPATH\.workspacer" -ItemType SymbolicLink -Target "C:$env:HOMEPATH\.dotfiles\windows\.workspacer" -Force
+New-Item -Path "$env:USERPROFILE\.workspacer" -ItemType SymbolicLink -Target "$env:USERPROFILE\.dotfiles\windows\.workspacer" -Force
 Write-host "~\.workspacer has been linked to .dotfiles\windows\.workspacer"
 
 # git-bash, zsh and p10k
-New-Item -Path "C:$env:HOMEPATH\.bashrc" -ItemType SymbolicLink -Target "C:$env:HOMEPATH\.dotfiles\windows\bash\.bashrc" -Force
+New-Item -Path "$env:USERPROFILE\.bashrc" -ItemType SymbolicLink -Target "$env:USERPROFILE\.dotfiles\windows\bash\.bashrc" -Force
 Write-host "~\.bashrc has been linked to .dotfiles\windows\bash\.bashrc"
 
-New-Item -Path "C:$env:HOMEPATH\.bash_profile" -ItemType SymbolicLink -Target "C:$env:HOMEPATH\.dotfiles\windows\bash\.bashrc" -Force
+New-Item -Path "$env:USERPROFILE\.bash_profile" -ItemType SymbolicLink -Target "$env:USERPROFILE\.dotfiles\windows\bash\.bashrc" -Force
 Write-host "~\.bashrc has been linked to .dotfiles\windows\bash\.bashrc"
 
 #zsh
-New-Item -Path "C:$env:HOMEPATH\.zshrc" -ItemType SymbolicLink -Target "C:$env:HOMEPATH\.dotfiles\windows\zsh\.zshrc" -Force
+New-Item -Path "$env:USERPROFILE\.zshrc" -ItemType SymbolicLink -Target "$env:USERPROFILE\.dotfiles\windows\zsh\.zshrc" -Force
 Write-host "~\.zshrc has been linked to .dotfiles\windows\zsh\.zshrc"
 
 
 #p10k/powerlevel10k
-New-Item -Path "C:$env:HOMEPATH\.p10k.zsh" -ItemType SymbolicLink -Target "C:$env:HOMEPATH\.dotfiles\windows\zsh\.p10k.zsh" -Force
+New-Item -Path "$env:USERPROFILE\.p10k.zsh" -ItemType SymbolicLink -Target "$env:USERPROFILE\.dotfiles\windows\zsh\.p10k.zsh" -Force
 Write-host "~\.p10k.zsh has been linked to .dotfiles\windows\zsh\.p10k.zsh"
 
 
 #nushell
-New-Item -Path "$env:APPDATA\nushell" -ItemType SymbolicLink -Target "C:$env:HOMEPATH\.dotfiles\common\nushell" -Force
+New-Item -Path "$env:APPDATA\nushell" -ItemType SymbolicLink -Target "$env:USERPROFILE\.dotfiles\common\nushell" -Force
 Write-host "nushell has been linked to .dotfiles\common\nushell"
 
 
--- windows/powershell/profile.ps1
+++ windows/powershell/profile.ps1
@@ -4,33 +4,3 @@
 (& "C:\Users\mino29\anaconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | Invoke-Expression
 #endregion
 
-
-
-
-
-
-
-#terminal font
-
-# check if hack-nf is installed,
-# if not then download and install it
-
-#various powershell modules from the internet
-
-# check if multi modules is importable,
-# if not then download and install them
-
-#various powershell custom functions 
-
-# just leave them as they are
-
-#other terminal applications
-
-# check winget
-# check scoop
-# check choco
-
-#check python
-#check conda
-#check lua
-#check neovim
