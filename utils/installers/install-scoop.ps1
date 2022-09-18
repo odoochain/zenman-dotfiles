3f0601bcc933e8e8edec795178d37f52652b0b23
mino29 <42552577+mino29@users.noreply.github.com>
2022-09-18 15:38:34 +0800

    update scoop install

--- utils/installers/install-scoop.ps1
+++ utils/installers/install-scoop.ps1
@@ -24,10 +24,38 @@ Write-host "scoop has been add to path. You can run `scoop --version` to test it
 
 #TODO: add more buckets
 #TODO: test the buckets added successfully
-#TODO: install a bunch of apps from scoop
 
+# most basic scoop packages
+# need to simply these, way too many duplicate lines
+scoop install git
+scoop install 7zip
+scoop install sudo
+scoop install aria2
+scoop install dark
+scoop install lessmsi
+scoop install gcc
+scoop inistall adb
+scoop inistall busybox
+scoop inistall CascadiaCode-NF
+scoop inistall FiraCode-NF
+scoop inistall Hack-NF
+scoop inistall colortool
+scoop inistall ffmpeg
+scoop inistall figlet
+scoop inistall innounp
+scoop inistall iperf3
+scoop inistall lxrunoffline
+scoop inistall winfetch
+scoop inistall nodejs-lts
+scoop inistall python
+scoop inistall rclone
+scoop inistall rufus
+scoop inistall screentogif
 }
 
+
+# to unset scoop proxy
+# scoop config rm proxy
 
 # $Env:PATH = "C:\Users\mino29\AppData\Roaming\Python\Python39\Scripts;$Env:PATH"
 #
@@ -45,3 +73,18 @@ Try{
 Catch{
     scoop install git
     }
+
+Try{
+    7z
+    }
+Catch{
+    scoop install 7zip
+    }
+
+Try{
+    aria2c --version
+    }
+Catch{
+    scoop install aria2
+    }
+
