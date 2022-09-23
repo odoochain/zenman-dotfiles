a2b17710e2b1185f84576a3fc474b80d7e4c77d5
mino29 <42552577+mino29@users.noreply.github.com>
2022-09-23 13:55:01 +0800

    add fnm/npm install, fix clink oh-my-posh.lua

--- windows/cmd/oh-my-posh.lua
+++ windows/cmd/oh-my-posh.lua
@@ -1,4 +1,2 @@
--- load(io.popen('oh-my-posh --config="~/scoop/apps/oh-my-posh/current/themes/atomicBit.omp.json" --init --shell cmd'):read("*a"))()
--- load(io.popen('oh-my-posh --config="~/scoop/apps/oh-my-posh/current/themes/space.omp.json" --init --shell cmd'):read("*a"))()
--- load(io.popen('oh-my-posh --config="~/scoop/apps/oh-my-posh/current/themes/slimfat.omp.json" --init --shell cmd'):read("*a"))()
-load(io.popen('oh-my-posh --config="~/.dotfiles/windows/oh-my-posh/themes/nord.omp.json" --init --shell cmd'):read("*a"))()
+-- load(io.popen('oh-my-posh --config="~/.dotfiles/windows/oh-my-posh/themes/polarnord.omp.json" --init --shell cmd'):read("*a"))()
+load(io.popen('oh-my-posh --config="~/.dotfiles/windows/oh-my-posh/themes/pwsh10k_norse.omp.json" --init --shell cmd'):read("*a"))()
