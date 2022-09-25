f2270485266b6c0ef8d9691c71315c7714c2dab2
mino29 <42552577+mino29@users.noreply.github.com>
2022-09-26 05:59:31 +0800

    add windows theme and mouse cursor pack

---
+++ windows/windows-theme/mouse-cursors/vsbluecursors/README.md
@@ -0,0 +1,4 @@
+# Note
+
+This set is from deviant art. Credit to Vlad Sukhetskyi
+[source](https://www.deviantart.com/vladsukhetskyi)
---
+++ windows/windows-theme/mouse-cursors/vsbluecursors/install.ps1
@@ -0,0 +1,5 @@
+# this works with install.inf file to install/copy cursor files
+RUNDLL32.EXE SETUPAPI.DLL,InstallHinfSection DefaultInstall 132 ./install.inf
+
+
+# the script can either be .ps1, .bat or .cmd
