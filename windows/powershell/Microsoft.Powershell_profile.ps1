e3b15ad1412a5b6ed7c0f74968dedab29984527b
mino29 <42552577+mino29@users.noreply.github.com>
2022-11-30 23:06:31 +0800

    (chore)

--- windows/powershell/Microsoft.Powershell_profile.ps1
+++ windows/powershell/Microsoft.Powershell_profile.ps1
@@ -681,7 +681,6 @@ function flatten {
         )
     $exclude_ext = @(".mp4", ".mkv", ".srt", ".ts")
     Get-ChildItem -Path $Target -Recurse -File | Move-Item -Destination $Target -Force
-    Get-ChildItem -Path $path -Recurse | Where-Object { $exclude_ext -notcontains $_.Extension } | Remove-Item -Force
     #Get-ChildItem -Path $Target -Recurse -Exclude *.mp4, *.mkv | Remove-Item -Force
     Get-ChildItem -Path $Target -Recurse | foreach {
        if( $_.psiscontainer -eq $true){
@@ -694,6 +693,7 @@ function flatten {
           Write-Output "Removing Empty File $($_.FullName)"
           $_.FullName | Remove-Item -Force
        }
+    Get-ChildItem -Path $path -Recurse | Where-Object { $exclude_ext -notcontains $_.Extension } | Remove-Item -Force
        <#
        .NOTES
        This delete all files less than 70MB
