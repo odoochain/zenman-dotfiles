654ea4f4302f0f475c244bf95ecabe10c53ac183
mino29 <42552577+mino29@users.noreply.github.com>
2022-12-07 17:14:48 +0800

    add user ueli theme

--- windows/powershell/Microsoft.Powershell_profile.ps1
+++ windows/powershell/Microsoft.Powershell_profile.ps1
@@ -720,3 +720,21 @@ function cleanTorrentTrash {
 # The End Goal of this profile
 # Make it feature-rich
 # Make it run blazingly fast
+
+function Get-Temperature {
+    $t = Get-WmiObject MSAcpi_ThermalZoneTemperature -Namespace "root/wmi"
+
+    $currentTempKelvin = $t.CurrentTemperature / 10
+    $currentTempCelsius = $currentTempKelvin - 273.15
+
+    $currentTempFahrenheit = (9/5) * $currentTempCelsius + 32
+
+    return $currentTempCelsius.ToString() + " C : " + $currentTempFahrenheit.ToString() + " F : " + $currentTempKelvin + "K"  
+}
+
+# Save in your c:\users\yourName\Documents\WindowsPowerShell\modules\ directory
+# in sub directory get-temperature as get-temperature.psm1
+# You **must** run as Administrator.
+# It will only work if your system & BIOS support it. If it doesn't work, I can't help you.
+
+# Just type get-temperature in PowerShell and it will spit back the temp in Celsius, Farenheit and Kelvin.
