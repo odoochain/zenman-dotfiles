4d55415234fe62639e49dd6ccb2df3fef4b2b4d5
mino29 <42552577+mino29@users.noreply.github.com>
2023-01-23 19:29:04 +0800

    delete unused files

--- windows/komorebi/install.ps1
+++
@@ -1,9 +0,0 @@
-# scoop bucket add extras
-# scoop install komorebi
-
-# disable win+L for lock screen
-
-# re-enable win+L for lock screen
-
-# source: https://superuser.com/questions/1059511/how-to-disable-winl-in-windows-10
-komorebic start --await-configuration
