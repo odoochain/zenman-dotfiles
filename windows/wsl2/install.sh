9440342d10fc43e44e79c5f6d6da8ff152bb7e6e
mino29 <42552577+mino29@users.noreply.github.com>
2023-06-05 09:17:51 +0800

    chore

--- windows/wsl2/install.sh
+++ windows/wsl2/install.sh
@@ -1,10 +1,10 @@
 REMOTE=https://gitee.com/mirrors/oh-my-zsh.git sh -c "$(wget https://gitee.com/mirrors/oh-my-zsh/raw/master/tools/install.sh -O -)"
+
+$ZSH_CUSTOM="$home/.oh-my-zsh/custom"
+echo $ZSH_CUSTOM
+
 git clone https://gitee.com/githubClone/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://gitee.com/lightnear/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-
-
-# $ZSH_CUSTOM="$home/.oh-my-zsh/custom"
-# echo $ZSH_CUSTOM
 
 git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
