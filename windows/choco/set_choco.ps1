c21e223e8e22109921d7f3ae252413866420d0b4
mino29 <42552577+mino29@users.noreply.github.com>
2023-07-10 01:21:23 +0800

    remove unused files

--- windows/choco/set_choco.ps1
+++
@@ -1,1 +0,0 @@
-choco feature enable -n allowGlobalConfirmation
---
+++ windows/potplayer/set_potplayer.ps1
@@ -0,0 +1,8 @@
+
+$source_ini_path = "$HOME\.dotfiles\windows\potplayer\PotPlayer64.ini"
+$target_ini_path = "$HOME\scoop\persist\potplayer\PotPlayer64.ini"
+
+$source_skin_path = "$HOME\.dotfiles\windows\potplayer\Kardox.dsf"
+$target_skin_path = "$HOME\scoop\persist\potplayer\Kardox.dsf"
+
+Copy-Item -Path $source_path -Target $target_path -force
