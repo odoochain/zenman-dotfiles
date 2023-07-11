e79498a68fe13e6fc8b61032249f32ae992b9a8d
mino29 <42552577+mino29@users.noreply.github.com>
2023-07-11 21:06:18 +0800

    update set_potplayer.ps1

--- windows/potplayer/set_potplayer.ps1
+++ windows/potplayer/set_potplayer.ps1
@@ -2,7 +2,7 @@
 $source_ini_path = "$HOME\.dotfiles\windows\potplayer\PotPlayer64.ini"
 $target_ini_path = "$HOME\scoop\persist\potplayer\PotPlayer64.ini"
 
-$source_skin_path = "$HOME\.dotfiles\windows\potplayer\Kardox.dsf"
-$target_skin_path = "$HOME\scoop\persist\potplayer\Kardox.dsf"
+# $source_skin_path = "$HOME\.dotfiles\windows\potplayer\Kardox.dsf"
+# $target_skin_path = "$HOME\scoop\persist\potplayer\Kardox.dsf"
 
-Copy-Item -Path $source_path -Target $target_path -force
+Copy-Item -Path $source_ini_path -Destination $target_ini_path -force
