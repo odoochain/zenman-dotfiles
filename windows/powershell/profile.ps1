7229e904740136b8e44548a950ce8f38936ad70b
mino29 <42552577+mino29@users.noreply.github.com>
2023-09-07 20:05:55 +0800

    chore

--- windows/powershell/profile.ps1
+++
@@ -1,8 +0,0 @@
-
-#region conda initialize
-# !! Contents within this block are managed by 'conda init' !!
-If (Test-Path "$home\scoop\apps\anaconda3\current\App\Scripts\conda.exe") {
-    (& "$home\scoop\apps\anaconda3\current\App\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
-}
-#endregion
-
