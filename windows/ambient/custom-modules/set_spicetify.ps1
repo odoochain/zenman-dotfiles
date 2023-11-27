1227f7a2994298adb987a5008b34b03649bc3062
mino29 <42552577+mino29@users.noreply.github.com>
2023-11-27 16:52:46 +0800

    chore

---
+++ windows/ambient/custom-modules/set_spicetify.ps1
@@ -0,0 +1,12 @@
+# create a backup/restore point
+spicetify backup apply enable-devtools
+
+# choose a custom theme
+spicetify config current_theme text
+spicetify config color_scheme tokyonight
+
+# Use a custom extension
+spicetify config extensions shuffle+.js
+
+# apply all the mods
+spicetify apply
